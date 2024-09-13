2a42cd817b8dd255ff4baf25206a040e3d98874e
Kirito-kun228 <andreevpv76@gmail.com>
2024-09-13 22:50:28 +0300

    prod duild v1

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import threading
 import time
 import codecs
 import json
+from webbrowser import register
+
 import schedule
 import telebot
 import os
@@ -29,9 +31,6 @@ class User:
         self.max_score = max(self.max_score, self.score)
 
 
-DATA = []
-
-
 @bot.message_handler(commands=['help'])
 def start_help_func(message):
     bot.send_message(message.chat.id, "Доступные команды отображены на кнопках у вас на экране")
@@ -50,20 +49,33 @@ def start_func(message):
 
 @bot.message_handler(content_types=['text'])
 def chose_func(message):
-    if message.text == "Играть":
-        user = (User(user_id=message.chat.id, name=message.from_user.first_name, ))
+    # Получаем пользователя по ID
+    user=''
+    for i in DATA:
+        if int(i.user_id) == int(message.chat.id):
+            user = i
+    if user!='' and list(user.used_cities)!=list(''.split(",")):  # Если пользователь в игре, перенаправляем на функцию game
+        game(message, user)
+    elif message.text == "Играть":
+        user = User(user_id=message.chat.id, name=message.from_user.first_name)
+        info = cursor.execute('SELECT * FROM users WHERE user_id=?', (user.user_id,))
+        if info.fetchone() is None:
+            create_users = 'INSERT INTO users (user_id, name, used_cities, score, max_score, dificulty_level) VALUES (?, ?, ?, ?, ?, ?)'
+            data = [(user.user_id, user.name, ",".join(user.used_cities), user.score, user.max_score, user.dificulty_level)]
+            with connection:
+                connection.executemany(create_users, data)
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = types.KeyboardButton("Легко")
         btn2 = types.KeyboardButton("Сложно")
         btn3 = types.KeyboardButton("Невозможно")
         markup.add(btn1, btn2, btn3)
         bot.send_message(message.chat.id, 'Выбери уровень сложности', reply_markup=markup)
-
         bot.register_next_step_handler(message, dif_lvl, user)
     elif message.text == "Таблица рекордов":
-        records()
+        records(user)
     else:
         bot.send_message(message.chat.id, 'Сообщение не распознано')
+
 
 
 def dif_lvl(message, user):
@@ -107,12 +119,11 @@ def game(message, user):
         user.score += 1
         bot_game(message, user)
     elif message.text.capitalize() == "Сдаться":
-        bot.send_message(message.chat.id, 'Ха-Ха-Ха Я снова победил!')
-        bot.register_next_step_handler(message, final, user)
-    elif str(message.text.capitalize()) in user.used_cities:
+        final(user,0)
+    elif user.score != 0 and str(message.text.capitalize()) in user.used_cities:
         bot.send_message(message.chat.id, 'Этот город уже был, попробуй еще раз')
         bot.register_next_step_handler(message, game, user)
-    elif city[0] != user.used_cities[-1][-1].capitalize() and city[0] != user.used_cities[-1][-2].capitalize():
+    elif user.score != 0 and city[0] != user.used_cities[-1][-1].capitalize() and city[0] != user.used_cities[-1][-2].capitalize():
         bot.send_message(message.chat.id, 'Город не на ту букву, попробуй еще раз')
         bot.register_next_step_handler(message, game, user)
     else:
@@ -122,7 +133,7 @@ def game(message, user):
 
 def bot_game(message, user):
     if user.score > user.dificulty_level:
-        bot.send_message(user.user_id, 'Ты победил! Игра окончена')
+        final(user,1)
     else:
         for i in data_cities:
             if (
@@ -136,7 +147,7 @@ def bot_game(message, user):
             ):
                 user.used_cities.append(i)
                 bot.send_message(user.user_id, f'Город: {i}\nТвой ход!')
-                bot.register_next_step_handler(message, game, user)
+                sev_game(user)
                 break
         else:
             for i in data_cities:
@@ -150,29 +161,55 @@ def bot_game(message, user):
                 ):
                     user.used_cities.append(i)
                     bot.send_message(user.user_id, f'Город: {i}\nТвой ход!')
-                    bot.register_next_step_handler(message, game, user)
+                    sev_game(user)
                     break
             else:
+                final(user,1)
 
-                bot.send_message(user.user_id, 'Ты победил! Игра окончена')
 
 
-def final(message, user):
+def final(user, win):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("Играть")
     btn2 = types.KeyboardButton("Таблица рекордов")
     markup.add(btn1, btn2)
-    bot.send_message(user.user_id,
+    user.used_cities=list()
+    user.max_score=max(user.score, user.max_score)
+    user.score=0
+    user.dificulty_level=0
+    update_users = 'UPDATE users SET used_cities=?, score=?, max_score=?, dificulty_level=? WHERE user_id=?'
+    data = [
+        (",".join(user.used_cities), user.score, user.max_score, user.dificulty_level, user.user_id)
+    ]
+    with connection:
+        connection.executemany(update_users, data)
+    if win == 1:
+        bot.send_message(user.user_id,
                      'Ты победил! Игра окончена. Можешь гордиться этим. Чтобы начать новую игру нажмите Играть снова',
                      reply_markup=markup)
+    else:
+        bot.send_message(user.user_id, 'Ха-Ха-Ха Я снова победил!')
+
+def records(user):
+    request_to_read_data = "SELECT name, max_score FROM users"
+    cursor = connection.cursor()
+    cursor.execute(request_to_read_data)
+    data = cursor.fetchall()
+    data.sort(key=lambda x: x[1])
+    str_records=''
+    for i in range(len(data)):
+        str_records=str_records+f'{i+1}. {data[i][0]} {data[i][1]} \n'
+    bot.send_message(user.user_id, f"Таблица рекордов:\n{str_records}")
 
 
-def records():
-    ...
-
-
-def sev_game():
-    ...
+def sev_game(user):
+    update_users = 'UPDATE users SET used_cities=?, score=?, max_score=?, dificulty_level=? WHERE user_id=?'
+    data = [
+        (",".join(user.used_cities), user.score, user.max_score, user.dificulty_level, user.user_id)
+    ]
+    with connection:
+        connection.executemany(update_users, data)
+    bot.register_next_step_handler_by_chat_id(user.user_id, game, user)
 
 
 def thread_func():
@@ -211,20 +248,38 @@ if __name__ == '__main__':
     CREATE TABLE IF NOT EXISTS users (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       user_id TEXT NOT NULL,
-      time TEXT NOT NULL,
-      weather INTEGER,
-      news INTEGER,
-      horoscope INTEGER
+      name TEXT NOT NULL,
+      used_cities TEXT,
+      score INTEGER,
+      max_score INTEGER,
+      dificulty_level INTEGER
     );
     """
     execute_query(connection, create_users_table)
 
     with codecs.open('cities.json', 'r', "utf_8_sig") as f:
         fcities = json.load(f)
-
     a = fcities['city']
     data_cities = list()
+
     for i in a:
         data_cities.append(str(i['name']))
 
+    DATA = []
+
+    request_to_read_data = "SELECT * FROM users"
+    cursor = connection.cursor()
+    cursor.execute(request_to_read_data)
+    data = cursor.fetchall()
+
+    for i in range(len(data)):
+        user = User(user_id=data[i][1],
+                    name=data[i][2],
+                    used_cities=list(data[i][3].split(",")),
+                    score=data[i][4],
+                    max_score=data[i][5],
+                    dificulty_level=data[i][6], )
+        DATA.append(user)
+
     bot.polling(none_stop=True)
+
